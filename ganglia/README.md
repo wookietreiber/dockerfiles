eb22e592b153e0d621a6f2d29f7b8f288fcd4829
Christian Krause <kizkizzbangbang@googlemail.com>
2013-12-25 23:55:20 +0100

    initial commit of working container

---
+++ ganglia/README.md
@@ -0,0 +1,21 @@
+To play around with, just the default configuration and just the container:
+
+    docker run -p 0.0.0.0:80:80 wookietreiber/ganglia
+
+To see some container usage help:
+
+    docker run wookietreiber/ganglia --help
+
+Add your configuration:
+
+    docker run -v /path/to/conf:/etc/ganglia -p 0.0.0.0:80:80 wookietreiber/ganglia
+
+I usually run it like this:
+
+    docker run -d \
+      -name ganglia \
+      -h my.fqdn.org \
+      -v /path/to/conf:/etc/ganglia \
+      -v /path/to/ganglia:/var/lib/ganglia \
+      -p 0.0.0.0:80:80 \
+      wookietreiber/ganglia
---
+++ ganglia/start.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+usage() { cat << EOF
+Usage: docker run wookietreiber/ganglia [opts]
+
+Run a ganglia-web container.
+
+    -? | -h | -help | --help            print this help
+    --with-gmond                        also run gmond inside the container
+    --without-gmond                     do not run gmond inside the container
+
+EOF
+}
+
+while true ; do
+  case "$1" in
+    -?|-h|-help|--help)
+      usage
+      exit 0
+      ;;
+
+    --with-gmond)
+      WITH_GMOND=y
+      shift
+      ;;
+
+    --without-gmond)
+      unset WITH_GMOND
+      shift
+      ;;
+
+    "")
+      break
+      ;;
+
+    *)
+      usage > /dev/stderr
+      exit 1
+      ;;
+  esac
+done
+
+set -x
+
+# --------------------------------------------------------------------------------------------------
+# preparation
+# --------------------------------------------------------------------------------------------------
+
+# just to be on the safe side
+mkdir -p /var/lib/ganglia/rrds
+
+# make sure 'nobody' owns the rrds or else gmetad will complain (this is needed explicitly here, in
+# case a bind mount from the host is used, i.e. docker run -v /path/to:/var/lib/ganglia ...)
+chown -R nobody:nobody /var/lib/ganglia/rrds
+
+# --------------------------------------------------------------------------------------------------
+# services
+# --------------------------------------------------------------------------------------------------
+
+service httpd start
+
+[[ -n $WITH_GMOND ]] && gmond -p /var/run/gmond.pid
+
+# last command must stay in foreground, this is the main reason for -d 1
+gmetad -d 1 -p /var/run/gmetad.pid
